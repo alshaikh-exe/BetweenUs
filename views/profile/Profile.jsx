2f5bf5a9426b4fee64deb541655c9fefb75d0d6f
abdulla <alshaikhabdulla6@gmail.com>
2025-08-02 21:45:53 +0300

    added back to home page link

--- views/profile/Profile.jsx
+++ views/profile/Profile.jsx
@@ -4,6 +4,7 @@ function Profile (props) {
     const user = props.user;
     return (
         <div>
+            <a href="/">Back to home page</a>
             <h1>{user.name}'s Page</h1>
         </div>
     )
