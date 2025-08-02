8d57feb0c0a50d6230cda75984e88c9167eb71de
abdulla <alshaikhabdulla6@gmail.com>
2025-08-02 12:05:33 +0300

    created the reply model

---
+++ models/reply.js
@@ -0,0 +1,15 @@
+const mongoose = require("mongoose");
+
+const replySchema = mongoose.Schema({
+    text: { type: String, required: true },
+    image: { type: String },
+    post: { type: mongoose.Schema.Types.ObjectId, ref: "Post", required: true },
+    author: { type: mongoose.Schema.Types.ObjectId, ref: "Author", required: true},
+},
+{
+    timestamps: true,
+});
+
+const Reply = mongoose.model("Reply", replySchema);
+
+module.exports = Reply;
