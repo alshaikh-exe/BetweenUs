e641db27be3b8bbdd9c7243bc99514d09992c37a
abdulla <alshaikhabdulla6@gmail.com>
2025-08-02 13:56:10 +0300

    created the sign up page

---
+++ views/auth/SignUp.jsx
@@ -0,0 +1,29 @@
+const React = require("react");
+
+function SignUp (props) {
+    return(
+        <div>
+            <h1>Sign Up</h1>
+            <form action="/users" method="POST">
+                Full Name: <input type="text" name="name" />
+                <br/>
+                Email: <input type="email" name="email" />
+                <br/>
+                Password: <input type="text" name="password" />
+                <br/>
+                Color: <select name="color" id="user-color">
+                    <option value="">Select your color!</option>
+                    <option value="#ff595e">Red</option>
+                    <option value="#ffca3a">Yellow</option>
+                    <option value="#8ac926">Green</option>
+                    <option value="#1982c4">Blue</option>
+                    <option value="#6a4c93">Purple</option>
+                    <option value="#292f36">Black</option>
+                </select>
+                <input type="submit" value="Create User" />
+            </form>
+        </div>
+    )
+}
+
+module.exports = SignUp;
