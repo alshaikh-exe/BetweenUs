66916c4cd700a1825d6554b085322ca4bcca520b
abdulla <alshaikhabdulla6@gmail.com>
2025-08-06 01:21:08 +0300

    styled edit reply page

--- views/replies/Edit.jsx
+++ views/replies/Edit.jsx
@@ -1,28 +1,43 @@
 const React = require("react");
-const Layout = require("layout");
+const Layout = require("../Layouts/Layout");
 
 function Edit (props) {
     const reply = props.reply;
     const token = props.token;
+    const user = props.user;
     return (
-        <div>
-                        <a href={`/?token=${token}`}>Go back to Home Page</a>
-            <h1>Edit Reply</h1>
+       <Layout>
+        <div className="edit-reply-container">
+            <h1 className="edit-reply-title">Edit Reply</h1>
+                        <a href={`/?token=${token}`} className="back-link">Go back to Home Page</a>
 
-            <div>
-                <form action={`/post/${reply.post._id}/replies/${reply._id}?_method=PUT&token=${token}`} method="POST">
-                <input type="text" name="text" defaultValue={reply.text} />
-                <input type="text" name="image" placeholder="Image URL" defaultValue={reply.image} />
-                <button type="submit">Update Reply</button>
-                </form>
-            </div>
+            <form 
+              action={`/post/${reply.post._id}/replies/${reply._id}?_method=PUT&token=${token}`} 
+              method="POST"
+              className="edit-reply-form"
+            >
+                <div className="form-section">
+                    <label>Reply Text</label>
+                    <input 
+                        type="text" 
+                        name="text" 
+                        defaultValue={reply.text} 
+                        required 
+                    />
+                </div>
 
-            <div>
-                <form action={`/post/${reply.post._id}/replies/${reply._id}?_method=DELETE&token=${token}`} method="POST">
-                <button type="submit">Delete</button>
-                </form>
-            </div>
+                <button type="submit" className="btn update-btn" style={{ backgroundColor: user.color }}>Update Reply</button>
+            </form>
+
+            <form 
+              action={`/post/${reply.post._id}/replies/${reply._id}?_method=DELETE&token=${token}`} 
+              method="POST"
+              className="delete-reply-form"
+            >
+                <button type="submit" className="btn delete-btn" style={{ backgroundColor: user.color }}>Delete Reply</button>
+            </form>
         </div>
+        </Layout>
     )
 }
 
